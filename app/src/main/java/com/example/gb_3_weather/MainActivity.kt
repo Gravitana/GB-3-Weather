6d3e3439e7e8c733b48dcbcfba98831a72b70e58
Vitaliy <v.a.grahov@ya.ru>
2021-06-03 21:57:39 +0500

    Init

---
+++ app/src/main/java/com/example/gb_3_weather/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gb_3_weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gb_3_weather.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gb_3_weather/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.gb_3_weather.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
